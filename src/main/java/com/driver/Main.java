088fdcd1d4d4be0e75f9432fb30973ee87309e66
Reshwanth <rachutireshwanth@gmail.com>
2022-12-23 22:28:24 +0530

    done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,7 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
-    System.out.println(dp.getPrice());
-    dp.addTakeaway();
-    dp.addExtraCheese();
-    dp.addTakeaway();
+    DeluxePizza dp = new DeluxePizza(false);
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
@@ -14,5 +10,6 @@ public class Main {
     p.addExtraToppings();
     p.addTakeaway();
     System.out.println(p.getBill());
+    System.out.println("Reshwanth");
   }
 }
